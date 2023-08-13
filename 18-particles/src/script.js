675359cce6ee514ba750fb4fa0ace6c1116c8383
seungwoohong <alwls601@naver.com>
2023-08-13 19:01:40 +0900

    Add completed particles

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -20,15 +20,57 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png');
 
+console.log(particleTexture)
 /**
- * Test cube
+ * Particles
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+const count = 2000;
+const positions = new Float32Array(count * 3) // 3 is x, y, z, like this [0,0,0 , 1,1,1 , 2,2,2], coordinate for vertax each 3 
+const colors = new Float32Array(count * 3) // [r,g,b , r,g,b]  like potions
+
+
+for (let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10;
+    colors[i] = Math.random();
+}
+
+const particlesGeometry = new THREE.BufferGeometry();
+const particlesMaterial = new THREE.PointsMaterial()
+
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+// Points
+particlesMaterial.size = 0.1
+particlesMaterial.sizeAttenuation = true;
+particlesMaterial.color = new THREE.Color('#ff88cc');
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+
+// particle map을 쓰면 texture 이미지에 edge가 보이는 버그가 있다. 
+// 이를 해결하기 위한 방법은 여러가지가 있다.
+
+// Solution 1
+// particlesMaterial.alphaTest = 0.001
+
+// Solution 2 - 이 방법은 다른 오브젝트가 있을 때 색깔이 다르게 렌더링 되는 오류가 발생한다.
+// particlesMaterial.depthTest = false
+
+// Solution 3
+particlesMaterial.depthWrite = false
+
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+
+const cube = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshStandardMaterial({
+    color: '#ffffff'
+}))
+
+scene.add(particles)
 
 /**
  * Sizes
@@ -83,6 +125,19 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update particles 
+    // particles.rotation.y = elapsedTime * 0.18;
+
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3;
+        const x = particlesGeometry.attributes.position.array[i3]; 
+        // Avoid, attributes 를 계속해서 update하는 것은 성능에 좋지 않음
+        // Solution is custom shader
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x);
+
+    }
+    particlesGeometry.attributes.position.needsUpdate = true;
 
     // Update controls
     controls.update()
